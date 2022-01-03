0c5826c25cbb529f01600c2dcf3b6d0f2debdea9
hftamayo <hftamayo@gmail.com>
2022-01-03 16:05:31 -0600

    updating a library on UserRepository

--- src/main/java/com/htamayo/sbcrashcourse/lendingengine/domain/repository/UserRepository.java
+++ src/main/java/com/htamayo/sbcrashcourse/lendingengine/domain/repository/UserRepository.java
@@ -2,10 +2,10 @@ package com.htamayo.sbcrashcourse.lendingengine.domain.repository;
 
 import com.htamayo.sbcrashcourse.lendingengine.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
